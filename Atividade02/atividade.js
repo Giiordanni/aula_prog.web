5257a11b29fa44a3b46ce6ab62cc89db6a2c9011
giordanni <giordanni.formiga@aluno.uepb.edu.br>
2024-09-19 12:19:36 -0300

    modificando o cep

--- Atividade02/atividade.js
+++ Atividade02/atividade.js
@@ -4,18 +4,16 @@ const logradouro = document.querySelector(".logradouro");
 const bairro = document.querySelector(".bairro");
 const cidade = document.querySelector(".cidade");
 const uf = document.querySelector(".uf");
+const menssagemErro = document.querySelector(".mens_erro");
 
 btnElement.addEventListener("click", async () => {
   const cepRequisicao = document.querySelector(".cepRequisicao").value;
 
-  
-  if(cepRequisicao.length !== 8){
-    throw new Error("Tamanho do cep fornecido inválido")
+  if (cepRequisicao.length !== 8) {
+    throw new Error("Tamanho do cep fornecido inválido. Deve conter 8 dígitos");
     return; //impedir a requisição de continuar
-    }
-
+  }
   try {
-
     const response = await fetch(
       `https://viacep.com.br/ws/${cepRequisicao}/json/`
     );
@@ -26,7 +24,13 @@ btnElement.addEventListener("click", async () => {
     bairro.innerHTML = data.bairro;
     cidade.innerHTML = data.localidade;
     uf.innerHTML = data.uf;
-  } catch (error) {
-    console.error("Erro aos buscar o cep", error);
+    menssagemErro.innerHTML = "";
+  } catch (e) {
+    cep.innerHTML = "";
+    logradouro.innerHTML = "";
+    bairro.innerHTML = "";
+    cidade.innerHTML = "";
+    uf.innerHTML = "";
+    menssagemErro.innerHTML = "CEP inválido";
   }
 });
